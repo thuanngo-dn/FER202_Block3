c4b644e286cf9bd2adc8a444a53e4bdbbf8194f7
Ngovanthuan123 <thuannvde170398@gmail.com>
2025-08-21 14:30:01 +0700

    update darkmore

--- FER202_Block3/slot16/slot16-ex2/src/App.js
+++ FER202_Block3/slot16/slot16-ex2/src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { CartProvider } from "./components/CartContext";
 import DishesList from "./components/DishesList";
 import Cart from "./components/Cart";
@@ -6,9 +6,14 @@ import dishes from "./data/data"; // 👉 import dữ liệu từ file riêng
 import "./styles.css";
 
 function App() {
+  const [darkMode, setDarkMode] = useState(false);
+
   return (
     <CartProvider>
-      <div className="App">
+      <div className={darkMode ? "App dark" : "App"}>
+        <button onClick={() => setDarkMode(!darkMode)} className="dark-btn">
+          {darkMode ? "Light Mode" : " Dark Mode"}
+        </button>
         <DishesList dishes={dishes} />
         <Cart />
       </div>
